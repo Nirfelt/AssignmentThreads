697b1b7fffa89c96fb24cfd22e2035e3620bfa15
Nirfelt <sebastian.nirfelt@gmail.com>
2014-11-26 17:55:43 +0100

    Ficed wav

--- Assignment1Threads/Assignment1Threads/MainForm.cs
+++ Assignment1Threads/Assignment1Threads/MainForm.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,24 +13,42 @@ namespace Assignment1Threads
 {
     public partial class MainForm : Form
     {
+        private ThreadDisplay display;
+        private ThreadMusic music;
+        private ThreadTriangle triangle;
+        Thread musicThread;
+
         public MainForm()
         {
             InitializeComponent();
+            display = new ThreadDisplay();
+            
+            triangle = new ThreadTriangle();
         }
 
         private void btnOpenMusic_Click(object sender, EventArgs e)
         {
-
+            string filePath = string.Empty;
+            openFileDialog1.Filter = "music files (*.mp3)(*.wav)|*.mp3;*.wav|All files (*.*)|*.*";
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                filePath = openFileDialog1.FileName;
+                music = new ThreadMusic(filePath);
+                lblFilePath.Text = filePath;
+            }
+            
         }
 
         private void btnPlayMusic_Click(object sender, EventArgs e)
         {
-
+            musicThread = new Thread(new ThreadStart(music.PlayMusic));
+            musicThread.Start();
         }
 
         private void btnStopMusic_Click(object sender, EventArgs e)
         {
-
+            musicThread.Abort();
         }
 
         private void btnStartDisplay_Click(object sender, EventArgs e)
--- Assignment1Threads/Assignment1Threads/ThreadMusic.cs
+++ Assignment1Threads/Assignment1Threads/ThreadMusic.cs
@@ -3,10 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Media;
+using System.IO;
+using System.Threading;
 
 namespace Assignment1Threads
 {
     public class ThreadMusic
     {
+        SoundPlayer snd = null;
+        string path = string.Empty;
+
+        public ThreadMusic(string path)
+        {
+            this.path = path;
+        }
+        public void PlayMusic()
+        {
+            try
+            {
+                snd = new SoundPlayer();
+                snd.SoundLocation = path;
+                snd.Load();
+                snd.Play();
+                while (true) ;
+            }
+            catch (ThreadAbortException)
+            {
+                snd.Stop();
+                //System.Windows.Forms.MessageBox.Show("Abort");
+                throw;
+            }
+        }
     }
 }
