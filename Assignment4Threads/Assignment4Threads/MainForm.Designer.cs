8c5e9408f8f4a101228db8189a57dc0c528401cf
Nirfelt <sebastian.nirfelt@gmail.com>
2015-01-12 15:40:17 +0100

    Added classes

--- Assignment4Threads/Assignment4Threads/MainForm.Designer.cs
+++ Assignment4Threads/Assignment4Threads/MainForm.Designer.cs
@@ -34,9 +34,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(784, 464);
             this.Name = "MainForm";
-            this.Text = "Form1";
+            this.Text = "Christamas gift delivery system";
             this.ResumeLayout(false);
 
         }
